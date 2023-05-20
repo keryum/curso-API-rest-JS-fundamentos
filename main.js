f2a9c4acd9eb9f0ddb67fa1f80f3ce114c6bf7a7
keryum <alejandro_suarezp@outlook.com>
2023-05-19 20:40:07 -0500

    a

--- main.js
+++ main.js
@@ -1,64 +1,109 @@
-const API_URL_RANDOM= "https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_jRjrmwvUgTc1KArB1PqQcAho9v6GGM9NcMr1gooi4XRYmaMPQn1HSulhiDFTPPWG";
-const API_URL_FAV= "https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_jRjrmwvUgTc1KArB1PqQcAho9v6GGM9NcMr1gooi4XRYmaMPQn1HSulhiDFTPPWG";
+const API_URL_RANDOM= "https://api.thecatapi.com/v1/images/search?limit=50&api_key=live_jRjrmwvUgTc1KArB1PqQcAho9v6GGM9NcMr1gooi4XRYmaMPQn1HSulhiDFTPPWG";
+const API_URL_FAV= "https://api.thecatapi.com/v1/favourites?api_key=live_jRjrmwvUgTc1KArB1PqQcAho9v6GGM9NcMr1gooi4XRYmaMPQn1HSulhiDFTPPWG";
+const API_URL_FAV_DELETE= (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_jRjrmwvUgTc1KArB1PqQcAho9v6GGM9NcMr1gooi4XRYmaMPQn1HSulhiDFTPPWG`;
 
-const addFavourite = document.querySelector('.addfavourite')
-const favourite = document.querySelector ('.favourite')
 const spanError = document.getElementById('error')
 
 async function loadRandomMichis() {
     const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
-
-        const img1 = document.querySelector('#img1')
-        const img2 = document.querySelector('#img2')
-        const img3 = document.querySelector('#img3')
-        const img4 = document.querySelector('#img4')
-        const img5 = document.querySelector('#img5')
-        const img6 = document.querySelector('#img6')
-        const img7 = document.querySelector('#img7')
-        const img8 = document.querySelector('#img8')
-        const img9 = document.querySelector('#img9')
-        const img10 = document.querySelector('#img10')
-        
-        img1.src = data[0].url
-        img2.src = data[1].url
-        img3.src = data[2].url
-        img4.src = data[3].url
-        img5.src = data[4].url
-        img6.src = data[5].url
-        img7.src = data[6].url
-        img8.src = data[7].url
-        img9.src = data[8].url
-        img10.src = data[9].url
-    
-};
-
-loadRandomMichis();
-loadFavouriteMichis();
-async function loadFavouriteMichis() {
-    const res = await fetch(API_URL_FAV);
+    console.log(data)
     if (res.status !==200) {
         spanError.classList.toggle('inactive')
         spanError.innerText= `Ups, algo salió mal 
         error ${res.status}` 
+    } else {
+        const article = document.querySelector('.randomMichisArticle')
+        article.innerText = '';
+        data.forEach(michi => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        const div = document.createElement('div');
+        const btnCreate = document.createElement('img');
+        const btnQuit = document.createElement('img')
+
+        btnCreate.src = './iconos/pngwing.com (7).png';
+        btnCreate.onclick = () => ambas();
+        btnQuit.src = './iconos/pngwing.com (6).png'
+        div.appendChild(btnCreate);
+        div.appendChild(btnQuit);
+        img.src = michi.url;
+        figure.appendChild(img);
+        figure.appendChild(div);
+        article.appendChild(figure);
+
+        btnCreate.classList.add('addfavourite')
+        btnQuit.classList.add('addfavourite', 'inactive')
+        img.width = 200;
+        img.height = 200;
+
+        function ambas() {
+            saveFavouriteMichis(michi.id);
+            btnChange();
+        }
+
+        function btnChange () {
+            btnCreate.classList.add('inactive')
+            btnQuit.classList.toggle('inactive')
+        }
+        });
     }
-    const data = await res.json();
 };
 
-async function saveFavouriteMichis() {
-    const rest = await fetch()
+
+async function loadFavouriteMichis() {
+    const res = await fetch(API_URL_FAV);
+    const data = await res.json();
+    if (res.status !==200) {
+        spanError.classList.toggle('inactive')
+        spanError.innerText= `Ups, algo salió mal 
+        error ${res.status}` 
+    } else {
+        const article = document.querySelector('.FavMichisArticle')
+        article.innerHTML = '';
+        data.forEach(michi => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const div = document.createElement('div');
+            const btnQuit = document.createElement('img');
+
+            btnQuit.src = './iconos/pngwing.com (6).png';
+            btnQuit.onclick = () => deleteFavouriteMichis(michi.id);
+            div.appendChild(btnQuit);
+            img.src = michi.image.url
+            figure.appendChild(img);
+            figure.appendChild(div);
+            article.appendChild(figure);
+
+            btnQuit.classList.add('addfavourite')
+            img.width = 200;
+            img.height = 200;
+        });
+    }
+    console.log(data);
+};
+async function saveFavouriteMichis(id) {
+    const res = await fetch(API_URL_FAV, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            image_id: id
+        }),
+    });
+    const data = await res.json();
+    console.log(data);
+    loadFavouriteMichis();
 }
 
-addFavourite.addEventListener('click', addToFavourites)
-favourite.addEventListener('click', noFavourite)
-
-function addToFavouritesBtn() {
-    addFavourite.classList.add('inactive')
-    favourite.classList.toggle('inactive')
+async function deleteFavouriteMichis(id) {
+    const res = await fetch(API_URL_FAV_DELETE(id), {
+        method: 'DELETE',
+    });
+    const data = await res.json();
+    loadFavouriteMichis();
 }
 
-function noFavourite() {
-    addFavourite.classList.toggle('inactive')
-    favourite.classList.add('inactive')
-    
-}
+    loadRandomMichis();
+    loadFavouriteMichis();
